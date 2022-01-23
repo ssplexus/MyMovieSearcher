293c5262932cc11cde87cf661bedabcfa2f8d180
Vasyutchenko Alexey <88984453+ssplexus@users.noreply.github.com>
2022-01-23 21:16:01 +0300

    Material refactor, add night mode

--- app/src/main/java/ru/ssnexus/mymoviesearcher/MainActivity.kt
+++ app/src/main/java/ru/ssnexus/mymoviesearcher/MainActivity.kt
@@ -12,13 +12,49 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        button_menu.setOnClickListener { Toast.makeText(this, button_menu.text,Toast.LENGTH_SHORT).show() }
-        button_fav.setOnClickListener { Toast.makeText(this, button_fav.text,Toast.LENGTH_SHORT).show() }
-        button_later.setOnClickListener { Toast.makeText(this, button_later.text,Toast.LENGTH_SHORT).show() }
-        button_compile.setOnClickListener { Toast.makeText(this, button_compile.text,Toast.LENGTH_SHORT).show() }
-        button_settings.setOnClickListener { Toast.makeText(this, button_settings.text,Toast.LENGTH_SHORT).show() }
+//        button_menu.setOnClickListener { Toast.makeText(this, button_menu.text,Toast.LENGTH_SHORT).show() }
+//        button_fav.setOnClickListener { Toast.makeText(this, button_fav.text,Toast.LENGTH_SHORT).show() }
+//        button_later.setOnClickListener { Toast.makeText(this, button_later.text,Toast.LENGTH_SHORT).show() }
+//        button_compile.setOnClickListener { Toast.makeText(this, button_compile.text,Toast.LENGTH_SHORT).show() }
+//        button_settings.setOnClickListener { Toast.makeText(this, button_settings.text,Toast.LENGTH_SHORT).show() }
+
+        initNavigation()
 
     }
 
+    fun initNavigation()
+    {
+        topAppBar.setOnMenuItemClickListener {
+            when (it.itemId) {
+                R.id.settings -> {
+                    Toast.makeText(this, "Настройки", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                else -> false
+            }
+        }
+
+        topAppBar.setNavigationOnClickListener {
+            Toast.makeText(this, "Когда-нибудь здесь будет навигация...", Toast.LENGTH_SHORT).show()
+        }
+        bottom_navigation.setOnNavigationItemSelectedListener {
+
+            when (it.itemId) {
+                R.id.favorites -> {
+                    Toast.makeText(this, "Избранное", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.watch_later -> {
+                    Toast.makeText(this, "Посмотреть похже", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.selections -> {
+                    Toast.makeText(this, "Подборки", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                else -> false
+            }
+        }
+    }
 
 }
