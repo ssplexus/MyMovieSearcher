1fd29753d5f92246ca386cd5564b90f4757204fd
Vasyutchenko Alexey <88984453+ssplexus@users.noreply.github.com>
2022-02-20 15:47:59 +0300

    Add home fragment, add details fragment, add onBackPressed handling

--- app/src/main/java/ru/ssnexus/mymoviesearcher/MainActivity.kt
+++ app/src/main/java/ru/ssnexus/mymoviesearcher/MainActivity.kt
@@ -4,11 +4,13 @@ import android.content.Intent
 import ru.ssnexus.mymoviesearcher.adapter.FilmListRecyclerAdapter
 import android.os.Bundle
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_main.*
+import ru.ssnexus.mymoviesearcher.fragments.HomeFragment
 import ru.ssnexus.mymoviesearcher.helper.ItemTouchHelperCallback
 import ru.ssnexus.mymoviesearcher.model.Film
 import ru.ssnexus.mymoviesearcher.model.Item
@@ -16,28 +18,69 @@ import ru.ssnexus.mymoviesearcher.model.decoration.TopSpacingItemDecoration
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var filmsAdapter: FilmListRecyclerAdapter
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         initNavigation()
+        //Запускаем фрагмент при старте
+        supportFragmentManager
+            .beginTransaction()
+            .add(R.id.fragment_placeholder, HomeFragment())
+            .addToBackStack(null)
+            .commit()
 
-        RV_Init(getMoviesDB())
     }
 
-    fun getMoviesDB(): List<Film>
-    {
-        return listOf(
-            Film(0,"Black Phone", R.drawable.black_phone, "After being abducted by a child killer and locked in a soundproof basement, a 13-year-old boy starts receiving calls on a disconnected phone from the killer's previous victims."),
-            Film(1,"Hannibal", R.drawable.hannibal,"Living in exile, Dr. Hannibal Lecter tries to reconnect with now disgraced F.B.I. Agent Clarice Starling, and finds himself a target for revenge from a powerful victim."),
-            Film(2,"Horse Whisperer", R.drawable.horse_whisperer,"The mother of a severely traumatized daughter enlists the aid of a unique horse trainer to help the girl's equally injured horse."),
-            Film(3,"Hostel", R.drawable.hostel,"Three backpackers head to a Slovak city that promises to meet their hedonistic expectations, with no idea of the hell that awaits them."),
-            Film(4,"Lost City", R.drawable.lost_city, "A reclusive romance novelist on a book tour with her cover model gets swept up in a kidnapping attempt that lands them both in a cutthroat jungle adventure."),
-            Film(5,"Saw",R.drawable.sawx,"As a deadly battle rages over Jigsaw's brutal legacy, a group of Jigsaw survivors gathers to seek the support of self-help guru and fellow survivor Bobby Dagen, a man whose own dark secrets unleash a new wave of terror."),
-            Film(6,"When Will I Be Loved", R.drawable.when_will_i_be_loved, "Feeling undervalued by her boyfriend, a young woman begins to explore her sexuality with other people.")
-        )
+    fun launchDetailsFragment(film: Film) {
+        //Создаем "посылку"
+        val bundle = Bundle()
+        //Кладем наш фильм в "посылку"
+        bundle.putParcelable("film", film)
+        //Кладем фрагмент с деталями в перменную
+        val fragment = DetailsFragment()
+        //Прикрепляем нашу "посылку" к фрагменту
+        fragment.arguments = bundle
+
+        //Запускаем фрагмент
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.fragment_placeholder, fragment)
+            .addToBackStack(null)
+            .commit()
+    }
+
+    override fun onBackPressed() {
+        if(supportFragmentManager.backStackEntryCount == 1)
+        {
+
+            AlertDialog.Builder(this)
+                .setTitle("Вы хотите выйти?")
+                .setIcon(R.drawable.ic_round_menu_24)
+                .setPositiveButton("Да") { _, _ ->
+                    super.onBackPressed()
+                    if(android.os.Build.VERSION.SDK_INT >= 21)
+                    {
+                        finishAndRemoveTask();
+                    }
+                    else if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.JELLY_BEAN)
+                    {
+                        finishAffinity();
+                    } else{
+                        finish()
+                    }
+                    System.exit(0);
+                }
+                .setNegativeButton("Нет") { _, _ ->
+
+                }
+                .show()
+        }
+        else
+            super.onBackPressed()
+
     }
 
     fun initNavigation()
@@ -81,42 +124,6 @@ class MainActivity : AppCompatActivity() {
                 else -> false
             }
         }
-    }
-
-    fun RV_Init(db : List<Item>){
-        //находим наш RV
-        main_recycler.apply {
-            //Инициализируем наш адаптер в конструктор передаем анонимно инициализированный интерфейс,
-            //оставим его пока пустым, он нам понадобится во второй части задания
-            filmsAdapter = FilmListRecyclerAdapter(object : FilmListRecyclerAdapter.OnItemClickListener{
-                override fun click(film: Film) {
-
-                    //Создаем бандл и кладем туда объект с данными фильма
-                    val bundle = Bundle()
-                    //Первым параметром указывается ключ, по которому потом будем искать, вторым сам
-                    //передаваемый объект
-                    bundle.putParcelable("film", film)
-                    //Запускаем наше активити
-                    val intent = Intent(this@MainActivity, DetailsActivity::class.java)
-                    //Прикрепляем бандл к интенту
-                    intent.putExtras(bundle)
-                    //Запускаем активити через интент
-                    startActivity(intent)
-                }
-            })
-            //Присваиваем адаптер
-            adapter = filmsAdapter
-            //Присвои layoutmanager
-            layoutManager = LinearLayoutManager(this@MainActivity)
-            //Применяем декоратор для отступов
-            val decorator = TopSpacingItemDecoration(8)
-            addItemDecoration(decorator)
-        }
-        //Кладем нашу БД в RV
-        filmsAdapter.addItems(db)
-
-        val itemTouchHelper = ItemTouchHelper(ItemTouchHelperCallback(filmsAdapter))
-        itemTouchHelper.attachToRecyclerView(main_recycler)
     }
 
 }
