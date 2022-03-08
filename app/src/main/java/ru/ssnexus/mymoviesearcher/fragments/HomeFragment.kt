3b3cec6a4f9310ad91ea0ac285ed6fe85c23f5a7
Vasyutchenko Alexey <88984453+ssplexus@users.noreply.github.com>
2022-03-08 10:39:27 +0300

    Add searchview with CollapsingToolbarLayout

--- app/src/main/java/ru/ssnexus/mymoviesearcher/fragments/HomeFragment.kt
+++ app/src/main/java/ru/ssnexus/mymoviesearcher/fragments/HomeFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -14,8 +15,8 @@ import ru.ssnexus.mymoviesearcher.R
 import ru.ssnexus.mymoviesearcher.adapter.FilmListRecyclerAdapter
 import ru.ssnexus.mymoviesearcher.helper.ItemTouchHelperCallback
 import ru.ssnexus.mymoviesearcher.model.Film
-import ru.ssnexus.mymoviesearcher.model.Item
 import ru.ssnexus.mymoviesearcher.model.decoration.TopSpacingItemDecoration
+import java.util.*
 
 
 /**
@@ -41,7 +42,38 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        RV_Init((requireActivity() as MainActivity).db.getDB())
+        val filmsDataBase = (requireActivity() as MainActivity).db.getDB()
+
+        search_view.setOnClickListener {
+            search_view.isIconified = false
+        }
+
+        //Подключаем слушателя изменений введенного текста в поиска
+        search_view.setOnQueryTextListener(object : SearchView.OnQueryTextListener{
+            //Этот метод отрабатывает при нажатии кнопки "поиск" на софт клавиатуре
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return true
+            }
+            //Этот метод отрабатывает на каждое изменения текста
+            override fun onQueryTextChange(newText: String?): Boolean {
+                //Если ввод пуст то вставляем в адаптер всю БД
+                if(newText == null) return false
+                if (newText.isEmpty()) {
+                    filmsAdapter.addItems(filmsDataBase)
+                    return true
+                }
+                //Фильтруем список на поискк подходящих сочетаний
+                val result = filmsDataBase.filter {
+                    //Чтобы все работало правильно, нужно и запрос, и имя фильма приводить к нижнему регистру
+                    it.title.lowercase(Locale.getDefault()).contains(newText.lowercase(Locale.getDefault()))
+                }
+                //Добавляем в адаптер
+                filmsAdapter.addItems(result)
+                return true
+            }
+        })
+
+        RV_Init(filmsDataBase)
     }
 
     fun RV_Init(db : List<Film>){
